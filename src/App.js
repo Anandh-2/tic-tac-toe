550b82a02aa6088947e77aa4eefc1278ac0421da
Anandh-2 <anandh02102004@gmail.com>
2024-07-30 00:10:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function Board(){
+  const [squares, setSquares]=useState([null,null,null,null,null,null,null,null,null]);
+  const [symbol,setSymbol]=useState('X');
+  function handleClick(index){
+      setSquares(prevSquares=>{
+      const newSquares=[...prevSquares];
+      if(squares[index]){
+        return newSquares;
+      }
+      newSquares[index]=symbol;
+      setSymbol((symbol==='X')?'O':'X');
+      return newSquares;
+      })}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <div className='board-row'>
+      <button onClick={()=>handleClick(0)}>
+      {squares[0]}
+      </button>
+      <button onClick={()=>handleClick(1)}>
+      {squares[1]}
+      </button>
+      <button onClick={()=>handleClick(2)}>
+      {squares[2]}
+      </button>
     </div>
+    <div className='board-row'>
+    <button onClick={()=>handleClick(3)}>
+      {squares[3]}
+      </button>
+      <button onClick={()=>handleClick(4)}>
+      {squares[4]}
+      </button>
+      <button onClick={()=>handleClick(5)}>
+      {squares[5]}
+      </button>
+    </div>
+    <div className='board-row'>
+    <button onClick={()=>handleClick(6)}>
+      {squares[6]}
+      </button>
+      <button onClick={()=>handleClick(7)}>
+      {squares[7]}
+      </button>
+      <button onClick={()=>handleClick(8)}>
+      {squares[8]}
+      </button>
+    </div>
+  </>
   );
 }
-
-export default App;
